41f5a0d652eb8dcc69d92eae8a493a2b268f9410
Shjani49 <shjani49@gmail.com>
2020-07-23 12:55:34 -0600

    Add 'response.status' check for error handling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-alert( "hello,World!" );
+/**
+ * AJAX Examples
+ */
+
+// Retrieving an external resource (text file.)
+fetch( "http://api.open-notify.org/astros.json" ) // Sends the request...
+// We can use .then() to parse the response.
+.then( response => {
+    if( response.status >= 200 && response.status <=299 )
+    { // 200 range status/response codes are SUCCESSES!
+         return response.json(); // Convert to JSON, and send to next step (.then())
+    }
+    else
+    { // If it is another range, we are unsuccessful...
+        throw Error ( response.statusText ); //Display a formal error message reporting the concern.s
+    }
+} )
+// Data has been formatted, let's have a look inside...
+.then( data => {
+    // Output data to console for testing.
+    console.log( data )
+})
